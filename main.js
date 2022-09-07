379450df120c1af838c43a0ca9ed04fb2e24b208
AMANAT122008 <89586113+AMANAT122008@users.noreply.github.com>
2022-09-07 19:42:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+song = "";
+lwx = 0;
+lwy = 0;
+rwx = 0;
+rwy = 0;
+scoreLeftWrist = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    poseNet.on("pose", gotPoses)
+
+}
+
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist" + scoreLeftWrist);
+
+
+        lwx = results[0].pose.leftWrist.x
+        lwy = results[0].pose.leftWrist.y
+        console.log("rwx = " + rwx + "rwy = " + rwy);
+
+
+
+        rwx = results[0].pose.rightWrist.x
+        rwy = results[0].pose.rightWrist.y
+        console.log("lwx = " + lwx + "lwy = " + lwx);
+    }
+}
+
+
+function modelLoaded() {
+    console.log("posenet is intialized");
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals / 500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
+
+
+}
+
+
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+}
+
+
+
